15fae0e5c6a96e5aa16147de54b2678c29eb3d53
Annelilleor <annelilleor27@gmail.com>
2025-04-08 10:31:00 +0200

    DB

---
+++ Bachelorprojekt/src/DatabaseConnection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/hst-25-st-6-6402";
+    private static final String USER = "hst-25-st-6-6402";
+    private static final String PASSWORD = "oadochooyuuthabahkai";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connection successful!");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
