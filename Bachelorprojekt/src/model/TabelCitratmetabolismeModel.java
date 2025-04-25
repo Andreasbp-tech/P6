60d52d4c55a01651e3eefcdcfdb8a7137462303e
Annelilleor <annelilleor27@gmail.com>
2025-04-25 12:55:29 +0200

    3 tabeller på generelt - mangler opdatering af citratmetabolisme ved indtastning
    Co-Authored-By: Andreasbp-tech <188033817+Andreasbp-tech@users.noreply.github.com>

--- Bachelorprojekt/src/model/TabelCitratmetabolismeModel.java
+++ Bachelorprojekt/src/model/TabelCitratmetabolismeModel.java
@@ -1,5 +1,64 @@
 package model;
 
-public class TabelCitratmetabolismeModel {
+import utilities.DatabaseConnection;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
+public class TabelCitratmetabolismeModel {
+    private List<String> timestamps;
+    private Object[][] data;
+    private static final Logger logger = Logger.getLogger(TabelCitratmetabolismeModel.class.getName());
+
+    public void fetchData(String cprNr) {
+        timestamps = new ArrayList<>();
+        List<Object[]> dataList = new ArrayList<>();
+        String[] rowNames = { "Calciumdosis", "Citratdosis" };
+
+        try {
+            Connection conn = DatabaseConnection.getConnection();
+            Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String query = "SELECT * FROM Citratmetabolisme WHERE CPR_nr = '" + cprNr
+                    + "' ORDER BY tidspunkt DESC LIMIT 24";
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                String tidspunkt = rs.getString("tidspunkt");
+                String timePart = tidspunkt.substring(11, 16);
+                timestamps.add(timePart);
+            }
+
+            java.util.Collections.reverse(timestamps);
+            rs.beforeFirst();
+
+            data = new Object[rowNames.length][timestamps.size() + 1];
+            for (int i = 0; i < rowNames.length; i++) {
+                data[i][0] = rowNames[i];
+            }
+
+            int colIndex = timestamps.size();
+            while (rs.next()) {
+                data[0][colIndex] = rs.getDouble("calciumdosis");
+                data[1][colIndex] = rs.getDouble("citratdosis");
+                colIndex--;
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+            logger.info("Citratmetabolisme data fetched for CPR: " + cprNr);
+        } catch (Exception e) {
+            logger.severe("Fejl ved hentning af citratmetabolisme-data: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public List<String> getTimestamps() {
+        return timestamps;
+    }
+
+    public Object[][] getData() {
+        return data;
+    }
 }
--- Bachelorprojekt/src/view/GenereltView.java
+++ Bachelorprojekt/src/view/GenereltView.java
@@ -1,7 +1,9 @@
-package view;
+/*package view;
 
 import controller.TabelCRRTController;
+import controller.TabelCitratmetabolismeController;
 import model.TabelCRRTModel;
+import model.TabelCitratmetabolismeModel;
 import view.TabelCRRTView;
 import model.ValgStueModel;
 import controller.TabelAGasController;
@@ -11,7 +13,6 @@ import utilities.HeaderPanelUtil;
 
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.BorderFactory;
 
 public class GenereltView {
     private JPanel panel;
@@ -73,6 +74,19 @@ public class GenereltView {
         this.tabelCRRTController.updateView(model.getCprNr());
         tablePanel.add(tabelView.getTablePanel());
 
+        // Citratmetabolisme section
+        JLabel citratLabel = new JLabel("Citratmetabolisme", SwingConstants.CENTER);
+        citratLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        citratLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        tablePanel.add(citratLabel);
+
+        TabelCitratmetabolismeModel citratModel = new TabelCitratmetabolismeModel();
+        TabelCitratmetabolismeView citratView = new TabelCitratmetabolismeView();
+        TabelCitratmetabolismeController citratController = new TabelCitratmetabolismeController(citratModel,
+                citratView);
+        citratController.updateView(model.getCprNr());
+        tablePanel.add(citratView.getTablePanel());
+
         // A-gas section
         JLabel agasLabel = new JLabel("A-gas", SwingConstants.CENTER);
         agasLabel.setFont(new Font("Arial", Font.BOLD, 16));
@@ -90,6 +104,145 @@ public class GenereltView {
         centerContainer.setLayout(new BoxLayout(centerContainer, BoxLayout.Y_AXIS));
         centerContainer.add(actionPanel);
         centerContainer.add(tablePanel);
+        panel.add(centerContainer, BorderLayout.CENTER);
+    }
+
+    public JPanel getPanel() {
+        return panel;
+    }
+
+    public JButton getStueButton() {
+        return stueButton;
+    }
+
+    public JButton getRegistrerParameterButton() {
+        return registrerParameterButton;
+    }
+
+    public JButton getSeOrdinationerButton() {
+        return seOrdinationerButton;
+    }
+
+    public TabelCRRTController getTabelCRRTController() {
+        return tabelCRRTController;
+    }
+}*/
+
+package view;
+
+import controller.TabelCRRTController;
+import controller.TabelCitratmetabolismeController;
+import controller.TabelAGasController;
+import model.TabelCRRTModel;
+import model.TabelCitratmetabolismeModel;
+import model.TabelAGasModel;
+import model.ValgStueModel;
+import utilities.HeaderPanelUtil;
+import view.TabelCRRTView;
+import view.TabelCitratmetabolismeView;
+import view.TabelAGasView;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GenereltView {
+    private JPanel panel;
+    private JButton stueButton;
+    private JButton patientButton;
+    private JButton cprButton;
+    private JButton registrerParameterButton;
+    private JButton seOrdinationerButton;
+
+    private TabelCRRTController tabelCRRTController;
+
+    public GenereltView(int valgtStue, ValgStueModel model) {
+        model.getPatientData(valgtStue);
+        panel = new JPanel(new BorderLayout());
+
+        // Header and top buttons with spacing
+        JPanel headerPanel = HeaderPanelUtil.createHeaderPanel();
+        JPanel topPanel = new JPanel(new GridLayout(1, 3, 10, 0));
+        topPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        Font buttonFont = new Font("Arial", Font.BOLD, 18);
+
+        stueButton = new JButton("Stue " + valgtStue);
+        stueButton.setFont(buttonFont);
+        patientButton = new JButton("Patient: " + model.getFornavn() + " " + model.getEfternavn());
+        patientButton.setFont(buttonFont);
+        cprButton = new JButton("CPR: " + model.getCprNr());
+        cprButton.setFont(buttonFont);
+        topPanel.add(stueButton);
+        topPanel.add(patientButton);
+        topPanel.add(cprButton);
+
+        JPanel combinedNorth = new JPanel(new BorderLayout());
+        combinedNorth.add(headerPanel, BorderLayout.NORTH);
+        combinedNorth.add(topPanel, BorderLayout.CENTER);
+        panel.add(combinedNorth, BorderLayout.NORTH);
+
+        // Action buttons
+        JPanel actionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 20, 10));
+        registrerParameterButton = new JButton("Registrer parametre");
+        registrerParameterButton.setFont(buttonFont);
+        seOrdinationerButton = new JButton("Se ordinationer");
+        seOrdinationerButton.setFont(buttonFont);
+        actionPanel.add(registrerParameterButton);
+        actionPanel.add(seOrdinationerButton);
+
+        // Tables container with weighted rows
+        JPanel tablesContainer = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1;
+
+        // CRRT section (weighty = 3)
+        gbc.gridy = 0;
+        gbc.weighty = 2.4;
+        JPanel crrtSection = new JPanel(new BorderLayout());
+        JLabel crrtLabel = new JLabel("CRRT-parametre", SwingConstants.CENTER);
+        crrtLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        crrtSection.add(crrtLabel, BorderLayout.NORTH);
+        TabelCRRTModel tabelModel = new TabelCRRTModel();
+        TabelCRRTView tabelView = new TabelCRRTView();
+        this.tabelCRRTController = new TabelCRRTController(tabelModel, tabelView);
+        this.tabelCRRTController.updateView(model.getCprNr());
+        crrtSection.add(tabelView.getTablePanel(), BorderLayout.CENTER);
+        tablesContainer.add(crrtSection, gbc);
+
+        // Citrat section (weighty = 1)
+        gbc.gridy = 1;
+        gbc.weighty = 0.7;
+        JPanel citratSection = new JPanel(new BorderLayout());
+        JLabel citratLabel = new JLabel("Citratmetabolisme", SwingConstants.CENTER);
+        citratLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        citratSection.add(citratLabel, BorderLayout.NORTH);
+        TabelCitratmetabolismeModel citratModel = new TabelCitratmetabolismeModel();
+        TabelCitratmetabolismeView citratView = new TabelCitratmetabolismeView();
+        TabelCitratmetabolismeController citratCtrl = new TabelCitratmetabolismeController(citratModel, citratView);
+        citratCtrl.updateView(model.getCprNr());
+        citratSection.add(citratView.getTablePanel(), BorderLayout.CENTER);
+        tablesContainer.add(citratSection, gbc);
+
+        // A-gas section (weighty = 1)
+        gbc.gridy = 2;
+        gbc.weighty = 1.7;
+        JPanel agasSection = new JPanel(new BorderLayout());
+        JLabel agasLabel = new JLabel("A-gas", SwingConstants.CENTER);
+        agasLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        agasSection.add(agasLabel, BorderLayout.NORTH);
+        TabelAGasModel agasModel = new TabelAGasModel();
+        TabelAGasView agasView = new TabelAGasView();
+        TabelAGasController agasCtrl = new TabelAGasController(agasModel, agasView);
+        agasCtrl.updateView(model.getCprNr());
+        agasSection.add(agasView.getTablePanel(), BorderLayout.CENTER);
+        tablesContainer.add(agasSection, gbc);
+
+        // Combine actionPanel and tablesContainer
+        JPanel centerContainer = new JPanel(new BorderLayout());
+        centerContainer.add(actionPanel, BorderLayout.NORTH);
+        centerContainer.add(tablesContainer, BorderLayout.CENTER);
         panel.add(centerContainer, BorderLayout.CENTER);
     }
 
